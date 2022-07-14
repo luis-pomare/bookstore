ecdd68053c7a879436900a48f6dc2da825b8b189
unknown <luispomare92@gmail.com>
2022-07-14 14:16:24 -0500

    Configure store to use thunk middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
@@ -7,6 +9,13 @@ const rootReducer = combineReducers({
   categories: categoriesReducer,
 });
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore(
+  {
+    reducer: rootReducer,
+  },
+  applyMiddleware(thunk),
+);
 
 export default store;
+
+// Api app ID: TcS591AyH90Qp264PnmD
