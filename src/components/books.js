e4c0fe6268ffeb0f061d113f533075c36d50ef4a
unknown <luispomare92@gmail.com>
2022-07-15 15:32:21 -0500

    Add useEfect for fetching initial API request

--- src/components/books.js
+++ src/components/books.js
@@ -1,9 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Book from './book';
 import Form from './form';
+import { booksEndpoint } from '../redux/configureStore';
+import { addBook } from '../redux/books/books';
 
 const Books = () => {
   const books = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch(booksEndpoint);
+      const books = await response.json();
+      const keys = Object.keys(books);
+
+      keys.forEach((key) => {
+        dispatch(addBook(books[key][0]));
+      });
+    };
+    fetchBooks();
+  }, []);
 
   return (
     <main>
