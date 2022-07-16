aa9ffbb2caccd89d880bec577109301018010d56
unknown <luispomare92@gmail.com>
2022-07-16 11:08:04 -0500

    Create xml elements to use html intead of screenshot

--- src/components/book.js
+++ src/components/book.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBook, removeBookThunk } from '../redux/books/books';
-import bookStatus from '../images/bookStatus.png';
+import completionIcon from '../images/completionIcon.png';
 
 const Book = (prop) => {
   const {
@@ -34,7 +34,26 @@ const Book = (prop) => {
           <li className="blueText bookButton">Edit</li>
         </ul>
       </div>
-      <img src={bookStatus} alt="bookStatus" id="bookImage" />
+      <div className="bookStatus bookAlign">
+        <span className="bookPercentage bookAlign">
+          <img
+            src={completionIcon}
+            alt="Completion icon"
+            className="completionIcon"
+          />
+          <div className="bookNumCompleted">
+            <h5>37 %</h5>
+            <p className="bookCompleted">Completed</p>
+          </div>
+        </span>
+        <span className="chapters">
+          <p className="currentChapter">CURRENT CHAPTER</p>
+          <h6> Chapter 3</h6>
+          <button className="chapterButton" type="button">
+            UPDATE PROGRESS
+          </button>
+        </span>
+      </div>
     </section>
   );
 };
